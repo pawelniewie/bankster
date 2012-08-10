ac1b1b5e58e895522713c0a76f3b8518f7841170
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2012-08-10 22:37:27 +0200

    cocoa project

---
+++ cocoa/Watch My Money/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Watch My Money
+//
+//  Created by Pawel Niewiadomski on 10.08.2012.
+//  Copyright (c) 2012 Pawel Niewiadomski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ cocoa/Watch My Money/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Watch My Money
+//
+//  Created by Pawel Niewiadomski on 10.08.2012.
+//  Copyright (c) 2012 Pawel Niewiadomski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
